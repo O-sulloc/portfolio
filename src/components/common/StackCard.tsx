01abd55302ac02ffc6164d3d238023eaeccdb6a9
O-sulloc <bujjaf@gmail.com>
2024-07-04 13:32:03 +0900

    feat: add stackCard

--- src/components/common/StackCard.tsx
+++ src/components/common/StackCard.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
 
 type StackCardProps = {
-  name: string;
-  icon: string;
-};
+  icon: string,
+  title: string,
+  desc: string,
+  stackList: string,
+}
 
+const StackCard = ({ icon, title, desc, stackList }: StackCardProps) => {
+  return (
+    <div className='stack-wrapper'>
+      <img src={icon} className='stack-icon' />
+      <h3 className='stack-title'> {title} </h3>
+      <p className='stack-desc'> {desc} </p>
+      <span className='stack-list'> {stackList} </span>
+    </div>
+  )
+}
+
+const stackFirst = [
+  "Ruby",
+  "Ruby On Rails",
+  "Java",
+  "Spring Boot",
+  "JavaScript",
+  "TypeScript",
+  "Svelte",
+  "React",
+  "Oracle",
+  "MySQL",
+  "PostgreSQL",
+  "Postman"
+]
+
+const StackSecond = [
+  "Git",
+  "GitHub Actions",
+  "GitLab CI/CD",
+  "AWS",
+  "Docker",
+  "Kubernetes",
+  "Linux",
+]
+
+export default StackCard;
--- src/components/layouts/Stack.tsx
+++ src/components/layouts/Stack.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import StackCard from '../common/StackCard';
+import FullStackIcon from '../../assets/stacks/reactjs.png'
+import DevOpsIcon from '../../assets/stacks/docker.png'
 
 const Stack = () => {
   return (
     <>
-      <div className="stack">
+      <section className="stack-section" id='stack'>
         <h1>stack</h1>
-      </div>
+        <div className='stack-container'>
+          <StackCard
+            icon={FullStackIcon} 
+            title={'Full Stack Web Development'} 
+            desc={'I have built a diverse array of web applications from scratch using modern technologies such as Java and React.'}
+            stackList={'Java, javascript'} 
+            
+          />
+
+          <StackCard 
+            icon={DevOpsIcon} 
+            title={'DevOps'} 
+            desc={'Experience with Docker, Kubernetes, and CI/CD pipelines.'}
+            stackList={'Docker, Kubernetes, Jenkins'}
+          />
+        </div>
+
+      </section>
     </>
   )
 }
