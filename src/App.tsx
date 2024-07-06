c2571d064258aa39dab8391f6122e9924c9bbb05
O-sulloc <bujjaf@gmail.com>
2024-07-06 15:21:20 +0900

    design: Add mui library

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,11 @@ import { Home, Navbar, Stack, Project, Experience, Contact } from './components/
 import './App.css';
 
 const App = () => {
-  // const [isDark, setIsDark] = useState(true); // default = dark
-
-  // const darkMode = () => {
-  //   setIsDark(!isDark);
-  //   document.body.classList.toggle('light-mode');
-  //   // toggle 메서드는 특정 클래스가 요소의 클래스 목록에 있으면 제거하고, 없으면 추가하는 기능을 수행
-  // }
 
   return (
     <div className="container">
-      <div className='blob'>
+      <div className='blob-wrapper'>
+        <div className='blob'></div>
       </div>
       <Navbar />
       <Home />
@@ -26,7 +20,3 @@ const App = () => {
 }
 
 export default App
-
-// 1. 화면 구성
-// 2. 배경 만들기(움직이는 공?)
-// 3. glassmorphism 효과 주기
