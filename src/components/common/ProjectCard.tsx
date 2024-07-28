11cd8772ae638387e98249d04fe25944c687b747
O-sulloc <bujjaf@gmail.com>
2024-07-28 22:32:14 +0900

    style: Add css to project card component

--- src/components/common/ProjectCard.tsx
+++ src/components/common/ProjectCard.tsx
@@ -1,5 +1,7 @@
-import { Chip, Stack } from "@mui/material";
+import { Chip, IconButton, Stack } from "@mui/material";
 import React from "react";
+import { GitHub } from '@mui/icons-material';
+import { WebAsset } from '@mui/icons-material';
 
 type ProjectCardProps = {
   thumb: string,
@@ -13,24 +15,40 @@ type ProjectCardProps = {
 const ProjectCard = ({ thumb, title, desc, stackList, siteLink, githubLink }: ProjectCardProps ) => {
   return (
     <div className="project-wrapper">
-      <img src={thumb} className="project-thumb" />
-      <h3 className="project-title">{title}</h3>
-      <p className="project-desc">{desc}</p>
-      <Stack direction="row" className='stack-list-wrapper'>
-        {stackList.map((stack) => (
-          <Chip 
-            className='stack-chip'
-            key={stack}
-            label={stack}
-            sx={{ 
-              textTransform: 'capitalize',
-              '& .MuiChip-root': { margin: '10px' }
-            }} 
-          />
-        ))}
-      </Stack>  
-      <a href={siteLink} className="project-site-link">site link</a>
-      <a href={githubLink} className="project-github-link">github link</a>
+      <div className="project-content">
+        <div>
+          <a href={siteLink}>
+            <img src={thumb} className="project-thumb" />
+          </a>
+        </div>
+        
+        <div className="project-body">
+          <h3 className="project-title">{title}</h3>
+          <p className="project-desc">{desc}</p>
+          <Stack direction="row" className='stack-list-wrapper'>
+            {stackList.map((stack) => (
+              <Chip 
+                className='stack-chip'
+                key={stack}
+                label={stack}
+                sx={{ 
+                  textTransform: 'capitalize',
+                  '& .MuiChip-root': { margin: '10px' }
+                }} 
+              />
+            ))}
+          </Stack>
+          <div className="project-links">
+            <IconButton aria-label='github' sx={{ "&:hover": { color: "black" } }} href={githubLink}>
+              <GitHub />
+            </IconButton>
+            <IconButton aria-label='site' sx={{ "&:hover": { color: "black" } }} href={siteLink}>
+              <WebAsset />
+            </IconButton>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
